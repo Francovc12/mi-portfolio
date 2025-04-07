1fdd5a3b25e9d882fb68d2d4b3933d540859c8e2
Francovc12 <ignafrancov@gmail.com>
2025-04-07 17:36:45 -0300

    add vitepress

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "My Portfolio",
+  description: "Is a portfolio of Ignacio Villca",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Inicio', link: '/' },
+      { text: 'Proyectos', link: '/proyectos' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Rutas',
+        items: [
+          { text: 'Proyectos', link: '/proyectos' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Francovc12' }
+    ]
+  }
+})
